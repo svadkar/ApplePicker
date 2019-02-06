895e209ea25896b15eac46cc8022c7408d4eece8
svadkar <svadkar725@gmail.com>
2019-02-06 10:50:42 -0500

    Added Harder Difficulty

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        //Get the current screen positon of the mouse from Input
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //The Camera's z position sets how far to push the mouse
+        mousePos2D.z = -Camera.main.transform.position.z;
+
+        //Convert the point from 2D screen space into 3D game world space
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //Move the x postion of this Basket to the x position of the mouse
+        Vector3 pos = this.transform.position;
+        pos.x = mousePos3D.x;
+        this.transform.position = pos;
+
     }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        //Find out what hit this basket
+        GameObject collidedWith = coll.gameObject;
+        if (collidedWith.tag == "Apple")
+        {
+            Destroy(collidedWith);
+        }
+    }
+
 }
